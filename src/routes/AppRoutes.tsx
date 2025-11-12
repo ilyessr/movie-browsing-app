a2751790bc12377f7551784e32bffc88fac3564a
Ilyess <rachedi.ilyess@gmail.com>
2025-11-12 16:11:20 +0100

    refactor: rename page files to use Page suffix

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,15 +1,15 @@
 // src/routes/AppRoutes.tsx
 import { Routes, Route } from "react-router-dom";
-import Home from "../pages/Home";
-import MovieDetails from "../pages/MovieDetails";
-import NotFound from "../pages/NotFound";
+import HomePage from "../pages/HomePage";
+import MovieDetailsPage from "../pages/MovieDetailsPage";
+import NotFoundPage from "../pages/NotFoundPage";
 
 const AppRoutes = () => {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/movie/:id" element={<MovieDetails />} />
-      <Route path="*" element={<NotFound />} />
+      <Route path="/" element={<HomePage />} />
+      <Route path="/movie/:id" element={<MovieDetailsPage />} />
+      <Route path="*" element={<NotFoundPage />} />
     </Routes>
   );
 };
