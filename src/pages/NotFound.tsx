64a428ff9acadd0e1c3468f0482532e668445afc
Ilyess <rachedi.ilyess@gmail.com>
2025-11-12 16:08:24 +0100

    new page

---
+++ src/pages/NotFound.tsx
@@ -0,0 +1,9 @@
+const NotFound = () => {
+  return (
+    <div className="flex items-center justify-center h-screen text-white bg-gray-900">
+      <h1 className="text-2xl font-bold">404 - Page not found</h1>
+    </div>
+  );
+};
+
+export default NotFound;
