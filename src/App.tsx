41c08fb6cc331e6b858630b1b1e993758bb40f7d
Ilyess <rachedi.ilyess@gmail.com>
2025-11-30 22:52:43 +0100

    chore: set router basename for GitHub Pages

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import AppRoutes from "./routes/AppRoutes";
 
 const App = () => {
   return (
-    <Router>
+    <Router basename={import.meta.env.BASE_URL}>
       <AppRoutes />
     </Router>
   );
